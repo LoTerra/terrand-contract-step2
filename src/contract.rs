988ae5ba7f6ae238df323c5a05e5875509b35643
0xantman <antho74830@gmail.com>
2021-02-06 19:08:00 +0100

    Work done... ADDED - Verification callback

--- src/contract.rs
+++ src/contract.rs
@@ -1,6 +1,6 @@
 use cosmwasm_std::{
-    to_binary, Api, Binary, Env, Extern, HandleResponse, InitResponse, LogAttribute, Querier,
-    StdResult, Storage,
+    to_binary, Api, Binary, CanonicalAddr, CosmosMsg, Env, Extern, HandleResponse, HumanAddr,
+    InitResponse, Querier, StdResult, Storage, WasmMsg,
 };
 
 use crate::msg::{ConfigResponse, HandleMsg, InitMsg, QueryMsg};
@@ -10,7 +10,7 @@ use groupy::CurveAffine;
 use paired::bls12_381::{Bls12, Fq12, G1Affine, G2Affine};
 use paired::{Engine, PairingCurveAffine};
 
-use drand_verify::{g1_from_variable, g2_from_variable, VerificationError};
+use drand_verify::{derive_randomness, g1_from_variable, g2_from_variable, VerificationError};
 
 // Note, you can use StdResult in some functions where you do not
 // make use of the custom errors
@@ -34,11 +34,16 @@ pub fn init<S: Storage, A: Api, Q: Querier>(
 
 pub fn handle<S: Storage, A: Api, Q: Querier>(
     deps: &mut Extern<S, A, Q>,
-    _env: Env,
+    env: Env,
     msg: HandleMsg,
 ) -> StdResult<HandleResponse> {
     match msg {
-        HandleMsg::Verify { signature, msg_g2 } => verify(deps, signature, msg_g2),
+        HandleMsg::Verify {
+            signature,
+            msg_g2,
+            worker,
+            round,
+        } => verify(deps, env, signature, msg_g2, worker, round),
     }
 }
 
@@ -78,10 +83,22 @@ fn verify_step2(
     Ok(fast_pairing_equality(&g1, &sigma, pk, msg_on_g2))
 }
 
+fn encode_msg(msg: QueryMsg, address: HumanAddr) -> StdResult<CosmosMsg> {
+    Ok(WasmMsg::Execute {
+        contract_addr: address,
+        msg: to_binary(&msg)?,
+        send: vec![],
+    }
+    .into())
+}
+
 pub fn verify<S: Storage, A: Api, Q: Querier>(
     deps: &mut Extern<S, A, Q>,
+    env: Env,
     signature: Binary,
     msg_g2: Binary,
+    worker: CanonicalAddr,
+    round: u64,
 ) -> StdResult<HandleResponse> {
     // Load state
     let state = config(&mut deps.storage).load()?;
@@ -90,14 +107,18 @@ pub fn verify<S: Storage, A: Api, Q: Querier>(
     let msg_to_g2affine = g2_from_variable(&msg_g2.as_slice()).unwrap();
     // Verify
     let is_valid = verify_step2(&pk_to_g1affine, &signature.as_slice(), &msg_to_g2affine).unwrap();
-
+    let randomness = derive_randomness(signature.as_slice());
+    let msg = QueryMsg::VerifyCallBack {
+        round,
+        randomness: Binary::from(randomness),
+        valid: is_valid,
+        worker,
+    };
+    let res = encode_msg(msg, env.message.sender)?;
     Ok(HandleResponse {
-        messages: vec![],
+        messages: vec![res.into()],
         data: None,
-        log: vec![LogAttribute {
-            key: "valid".to_string(),
-            value: is_valid.to_string(),
-        }],
+        log: vec![],
     })
 }
 
@@ -107,11 +128,18 @@ pub fn query<S: Storage, A: Api, Q: Querier>(
 ) -> StdResult<Binary> {
     let response = match msg {
         QueryMsg::Config {} => to_binary(&query_config(deps)?)?,
+        QueryMsg::VerifyCallBack { .. } => to_binary(&query_verify_callback(deps)?)?,
     };
     Ok(response)
 }
 
 fn query_config<S: Storage, A: Api, Q: Querier>(
+    deps: &Extern<S, A, Q>,
+) -> StdResult<ConfigResponse> {
+    let state = config_read(&deps.storage).load()?;
+    Ok(state)
+}
+fn query_verify_callback<S: Storage, A: Api, Q: Querier>(
     deps: &Extern<S, A, Q>,
 ) -> StdResult<ConfigResponse> {
     let state = config_read(&deps.storage).load()?;
@@ -126,20 +154,26 @@ mod tests {
 
     #[test]
     fn verify_test() {
-        let mut deps = mock_dependencies(0, &[]);
+        let mut deps = mock_dependencies(7, &[]);
 
         let init_msg = InitMsg {};
         init(&mut deps, mock_env("creator", &[]), init_msg).unwrap();
 
         let signature: Binary = hex::decode("a75c1b05446c28e9babb078b5e4887761a416b52a2f484bcb388be085236edacc72c69347cb533da81e01fe26f1be34708855b48171280c6660e2eb736abe214740ce696042879f01ba5613808a041b54a80a43dadb5a6be8ed580be7e3f546e").unwrap().into();
         let g2_binary = hex::decode("8332743e3c325954435e289d757183e9d3d0b64055cf7f8610b0823d6fd2c0ec2a9ce274fd2eec85875225f89dcdda710fb11cce31d0fa2b4620bbb2a2147502f921ceb95d29b402b55b69b609e51bb759f94c32b7da12cb91f347b12740cb52").unwrap();
-
+        println!("{}, {:?}", signature, g2_binary);
         let msg = HandleMsg::Verify {
             signature: signature,
             msg_g2: Binary::from(g2_binary),
+            worker: deps
+                .api
+                .canonical_address(&HumanAddr::from("address"))
+                .unwrap(),
+            round: 12323,
         };
 
         let res = handle(&mut deps, mock_env("address", &[]), msg.clone()).unwrap();
-        assert_eq!(1, res.log.len());
+        println!("{:?}", res);
+        assert_eq!(0, res.log.len());
     }
 }
--- src/msg.rs
+++ src/msg.rs
@@ -1,5 +1,5 @@
 use crate::state::State;
-use cosmwasm_std::Binary;
+use cosmwasm_std::{Binary, CanonicalAddr};
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
@@ -10,12 +10,24 @@ pub struct InitMsg {}
 pub enum QueryMsg {
     /// Get the config state
     Config {},
+    /// Not used to be call directly
+    VerifyCallBack {
+        round: u64,
+        randomness: Binary,
+        valid: bool,
+        worker: CanonicalAddr,
+    },
 }
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 #[serde(rename_all = "snake_case")]
 pub enum HandleMsg {
-    Verify { signature: Binary, msg_g2: Binary },
+    Verify {
+        signature: Binary,
+        msg_g2: Binary,
+        worker: CanonicalAddr,
+        round: u64,
+    },
 }
 // We define a custom struct for each query response
 pub type ConfigResponse = State;
